0de6765f0f35b7a56d9494e3658f3dde09accdce
abdou <abdoucisse91140@gmail.com>
2021-02-09 22:29:10 +0100

    Versioning 1 pour site en local projet-cycles

---
+++ projet-cycles/public/assets/js/home.js
@@ -0,0 +1,19 @@
+/*
+
+function account() {
+    $.get('/mon-compte/', function (data) {
+        $('body').html(data);
+    })
+}
+function login() {
+    $.get('/connexion', function (data) {
+        location.href('/connexion')
+    })
+}
+function logOut() {
+    $.get('/deconnexion/', function (data) {
+        $('body').html(data);
+    })
+}
+
+*/
