1e59ebe6a6241387450f4f43be28b846fee6f858
abdou <abdoucisse91140@gmail.com>
2021-02-24 15:34:39 +0100

    Vscode local serve doesn't connect

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '863b3a13355c9d884b9997461b8af2fcd0e42819',
+    'name' => 'root/projet-cycles',
+  ),
+  'versions' => 
+  array (
+    'root/projet-cycles' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '863b3a13355c9d884b9997461b8af2fcd0e42819',
+    ),
+  ),
+);
